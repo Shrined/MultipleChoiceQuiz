1b2c9dfc02d23439733ed6e207ca555c7f88ed82
luke mcguire <lukemcguire3@yahoo.com>
2019-01-01 03:59:55 -0600

    First Commit

---
+++ QuizMaker/QuizLogic.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace QuizMaker
+{
+    class QuizLogic
+    {
+
+
+        Scoring S = new Scoring();
+        static int answer;
+        static int choice;
+
+        string[] Questions = File.ReadAllLines("C:/FILE/PATH/Questions.txt");
+        string[] Answers = File.ReadAllLines("C:/FILE/PATH/Desktop/Answers.txt");
+        string[] Choices = File.ReadAllLines("C:/FILE/PATH/Choices.txt");
+
+
+        public void StartQuiz()
+        {
+
+            Question();
+
+        }
+
+        private void Question()
+        {
+            /*** Iterates through the array of questions. 
+             * Each iteration calls the UserInput method
+             * The GameOver method is called once the iteration is complete
+             * ***/
+            for (int i = 0; i < Questions.Length; i++)
+            {
+                Console.WriteLine(Questions[i]);
+                UserInput(S);
+            }
+
+            S.GameOver();
+
+
+        }
+
+        public void UserInput(Scoring s)
+        {
+            /*** Prints and iterates through the the list of choices the user has to answer
+             * utilizes the split method to organize the choices
+             ***/
+
+            String[] ChoiceList = Choices[choice].Split('-');
+
+            for (int i = 0; i < Choices.Length; i++)
+            {
+                Console.WriteLine(ChoiceList[i]);
+            }
+
+            /*** Take the users input in type string
+             *   checks to see if the user's input is the correct answer
+             *   utilizes the Scoring.score method to keep track of the score
+             * ***/
+
+            choice++;
+            string Input;
+            Input = Console.ReadLine();
+
+            if (Input.Equals(Answers[answer], StringComparison.CurrentCultureIgnoreCase))
+            {
+                s.Score(true);
+                answer++;
+            }
+            else
+            {
+                s.Score(false);
+                answer++;
+            }
+            
+
+        }
+
+
+
+
+    }
+}
---
+++ QuizMaker/Scoring.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMaker
+{
+    class Scoring
+    {
+        static int correct = 0;
+        static int Wrong = 0;
+
+        public void Score(bool flag)
+        {
+            /*** if the user inputs the right answer, the integer value 'correct' increments by one
+             * vice versa if the user enters the wrong answer
+             * ***/
+            if (flag)
+            {
+                correct++;
+                Console.WriteLine("You got the answer right! \n");
+            }
+            else
+            {
+                Wrong++;
+                Console.WriteLine("That is not the correct answer. \n");
+            }
+        }
+
+        public void GameOver()
+        {
+            // Calculates the final score once the game is over and displays it to the user
+            Console.WriteLine("Your final score is " + correct + "/" + (correct + Wrong) + "\n");
+        }
+
+
+    }
+}
